c339277ca4c9a357fb57bf51a564abd36ef957ef
RoundBearChoi <roundbeargames@gmail.com>
2023-04-09 20:27:22 +0900

    save

--- FightingGameProject/PlayerTestState.cpp
+++ FightingGameProject/PlayerTestState.cpp
@@ -17,18 +17,17 @@ namespace RB::Players
 		_customUpdater.SetSkipFrames(1);
 		_customUpdater.SetTargetObj(this);
 		_customUpdater.SetTargetFunction(&PlayerTestState::SomeFunc);
-		_customUpdater.CallTargetFunction();
 	}
 
 	void PlayerTestState::OnFixedUpdate()
 	{
-		iPlayer* player = RB::Controllers::GameplayControllers::PLAYER_CONTROLLER->GetPlayerOnStateMachineID(_stateMachine->GetID());
-
-		player->Move(olc::vi2d{ 1, 1 });
+		_customUpdater.CallTargetFunction();
 	}
 
 	void PlayerTestState::SomeFunc()
 	{
-		std::cout << "calling some delegated func.." << " statemachine id: " << _stateMachine->GetID() << std::endl;
+		iPlayer* player = RB::Controllers::GameplayControllers::PLAYER_CONTROLLER->GetPlayerOnStateMachineID(_stateMachine->GetID());
+		
+		player->Move(olc::vi2d{ 1, 1 });
 	}
 }

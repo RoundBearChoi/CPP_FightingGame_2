73aac7376965d5dd2b03affdae686fd7eb88e238
RoundBearChoi <roundbeargames@gmail.com>
2023-04-09 11:38:59 +0900

    save

---
+++ FightingGameProject/PlayerDebugController.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "iPlayerDebugController.h"
+
+namespace RB::PlayerDebug
+{
+	class PlayerDebugController : public iPlayerDebugController
+	{
+
+	};
+}
---
+++ FightingGameProject/iPlayerDebugController.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "iController.h"
+
+using namespace RB::Controllers;
+
+namespace RB::PlayerDebug
+{
+	class iPlayerDebugController : public iController
+	{
+	public:
+
+	};
+}

35ac6c996d74b9209b0d1482eecbdb6fe313cb49
RoundBearChoi <roundbeargames@gmail.com>
2023-04-10 18:53:09 +0900

    save

--- FightingGameProject/TestUpdater.cpp
+++ FightingGameProject/TestUpdater.cpp
@@ -25,11 +25,6 @@ namespace RB::Updaters
 	void TestUpdater::OnUpdate()
 	{
 		RB::Controllers::ControllerGroup::UpdateAll();
-
-		olc::Renderer::ptrPGE->DrawLine(10, 10, 400, 10, olc::YELLOW);
-		olc::Renderer::ptrPGE->DrawLine(10, 10, 10, 300, olc::YELLOW);
-		olc::Renderer::ptrPGE->DrawLine(400, 10, 400, 300, olc::YELLOW);
-		olc::Renderer::ptrPGE->DrawLine(10, 300, 400, 300, olc::YELLOW);
 	}
 
 	void TestUpdater::OnFixedUpdate()

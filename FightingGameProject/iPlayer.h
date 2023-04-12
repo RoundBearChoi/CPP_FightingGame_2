1a972986f42be9d3269c6bdff5e03f1c364db579
RoundBearChoi <roundbeargames@gmail.com>
2023-04-12 13:12:18 +0900

    save

--- FightingGameProject/iPlayer.h
+++ FightingGameProject/iPlayer.h
@@ -10,11 +10,11 @@ namespace RB::Players
 		virtual ~iPlayer() {};
 
 	public:
-		virtual olc::vi2d GetPosition() = 0;
 		virtual olc::vi2d GetPlayerBox() = 0;
 		virtual PlayerID GetPlayerID() = 0;
 		virtual int GetStateMachineID() = 0;
 		virtual void Move(olc::vi2d moveAmount) = 0;
+		virtual olc::vi2d GetPosition() = 0;
 		virtual void SetPosition(olc::vi2d pos) = 0;
 	};
 }

103b9ef2209423aa44d8955be74a337cba2fa2b7
RoundBearChoi <roundbeargames@gmail.com>
2023-04-12 13:15:34 +0900

    save

---
+++ FightingGameProject/AABB.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace RB::Collisions
+{
+	class AABB
+	{
+	public:
+		AABB(float x, float y, float width, float height)
+		{
+			_x = x;
+			_y = y;
+			_width = width;
+			_height = height;
+		}
+
+		~AABB()
+		{
+
+		}
+
+	private:
+		float _x = 0.0f;
+		float _y = 0.0f;
+		float _width = 0.0f;
+		float _height = 0.0f;
+	};
+}

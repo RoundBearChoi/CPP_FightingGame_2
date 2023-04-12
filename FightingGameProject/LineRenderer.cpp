529dea3a9fd5ff4ad81d45f352d921ffa8eb4fd7
RoundBearChoi <roundbeargames@gmail.com>
2023-04-12 13:37:20 +0900

    save

---
+++ FightingGameProject/LineRenderer.cpp
@@ -0,0 +1,14 @@
+#include "LineRenderer.h"
+
+namespace RB::Render
+{
+	LineRenderer::LineRenderer()
+	{
+
+	}
+
+	LineRenderer::~LineRenderer()
+	{
+
+	}
+}
---
+++ FightingGameProject/LineRenderer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace RB::Render
+{
+	class LineRenderer
+	{
+	public:
+		LineRenderer();
+		~LineRenderer();
+	};
+}

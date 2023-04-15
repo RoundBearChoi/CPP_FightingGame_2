fdd19168b5af9e51295b0c8fa61bdf11e55d0b10
RoundBearChoi <roundbeargames@gmail.com>
2023-04-15 12:50:59 +0900

    save

--- FightingGameProject/Idle.cpp
+++ FightingGameProject/Idle.cpp
@@ -24,6 +24,8 @@ namespace RB::PlayerStates
 	{
 		UpdateStateComponents();
 
+		//temp
+
 		if (_stateMachine->IsTransitioning())
 		{
 			return;
@@ -32,8 +34,6 @@ namespace RB::PlayerStates
 		RB::Players::iPlayer* owner = GetOwnerPlayer();
 		
 		olc::HWButton jump = RB::Controllers::GameplayControllers::INPUT_CONTROLLER->GetButton(owner->GetPlayerID(), RB::Input::PlayerInput::JUMP);
-		//olc::HWButton moveLeft = RB::Controllers::GameplayControllers::INPUT_CONTROLLER->GetButton(owner->GetPlayerID(), RB::Input::PlayerInput::MOVE_LEFT);
-		//olc::HWButton moveRight = RB::Controllers::GameplayControllers::INPUT_CONTROLLER->GetButton(owner->GetPlayerID(), RB::Input::PlayerInput::MOVE_RIGHT);
 		
 		if (jump.bPressed || jump.bHeld)
 		{
@@ -41,41 +41,6 @@ namespace RB::PlayerStates
 		
 			return;
 		}
-
-		//if (moveLeft.bPressed && moveRight.bPressed)
-		//{
-		//	// do nothing
-		//}
-		//else if (moveLeft.bPressed)
-		//{
-		//	_stateMachine->QueueNextState(new RB::PlayerStates::MoveBack());
-		//
-		//	return;
-		//}
-		//else if (moveRight.bPressed)
-		//{
-		//	_stateMachine->QueueNextState(new RB::PlayerStates::MoveForward());
-		//
-		//	return;
-		//}
-		//
-		////temp
-		//if (moveLeft.bHeld && moveRight.bHeld)
-		//{
-		//	// do nothing
-		//}
-		//else if (moveLeft.bHeld)
-		//{
-		//	_stateMachine->QueueNextState(new RB::PlayerStates::MoveBack());
-		//
-		//	return;
-		//}
-		//else if (moveRight.bHeld)
-		//{
-		//	_stateMachine->QueueNextState(new RB::PlayerStates::MoveForward());
-		//
-		//	return;
-		//}
 	}
 
 	void Idle::OnFixedUpdate()

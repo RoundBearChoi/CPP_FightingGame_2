f5ca1d36b456e7d66af4884e85479a8a044dd278
RoundBearChoi <roundbeargames@gmail.com>
2023-05-10 16:58:21 +0900

    hurtbox dataset

---
+++ FightingGameProject/HurtBoxData.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace RB::HurtbOX
+{
+	class HurtBoxData
+	{
+		public:
+		HurtBoxData() = default;
+		~HurtBoxData() = default;
+
+		int mX = 0;
+		int mY = 0;
+		int mWidth = 0;
+		int mWeight = 0;
+	};
+}

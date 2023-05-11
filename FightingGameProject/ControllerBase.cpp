3234e52132ef311df4b8bfa7b89cfaf992395b96
RoundBearChoi <roundbeargames@gmail.com>
2023-05-11 21:31:33 +0900

    hasher

--- FightingGameProject/ControllerBase.cpp
+++ FightingGameProject/ControllerBase.cpp
@@ -4,7 +4,11 @@ namespace RB::Controllers
 {
 	void ControllerBase::SetHash(std::string str)
 	{
-		_hash = std::hash<std::string>{}(str);
+		//_hash = std::hash<std::string>{}(str);
+
+		std::hash<std::string> hasher;
+
+		_hash = hasher(str);
 	}
 
 	size_t ControllerBase::GetHash()

383704dad10295fce58a292bf9b8f087adaea298
RoundBearChoi <roundbeargames@gmail.com>
2023-05-13 22:52:21 +0900

    Revert "save"
    This reverts commit 32a1cf26bf20c5842b1abcabc9fd238995f49970.

--- FightingGameProject/HurtBoxDataController.cpp
+++ FightingGameProject/HurtBoxDataController.cpp
@@ -2,21 +2,13 @@
 
 namespace RB::HurtBox
 {
-	HurtBoxDataController::~HurtBoxDataController()
-	{
-		for (size_t i = 0; i < _vecData.size(); i++)
-		{
-			delete[] _vecData[i];
-		}
-	}
-
 	void HurtBoxDataController::Init()
 	{
 		_loader.Init();
 
 		HurtBoxData* arr = _loader.LoadData("HurtBoxData/Sample.HurtBoxData", 0);
 
-		_vecData.push_back(arr);
+		delete arr;
 	}
 
 	void HurtBoxDataController::OnUpdate()
--- FightingGameProject/HurtBoxDataController.h
+++ FightingGameProject/HurtBoxDataController.h
@@ -8,7 +8,7 @@ namespace RB::HurtBox
 	{
 	public:
 		HurtBoxDataController() = default;
-		~HurtBoxDataController() override;
+		~HurtBoxDataController() override {};
 
 	public:
 		void Init() override;
@@ -17,8 +17,5 @@ namespace RB::HurtBox
 
 	private:
 		HurtBoxDataLoader _loader;
-
-	private:
-		std::vector<HurtBoxData*> _vecData;
 	};
 }

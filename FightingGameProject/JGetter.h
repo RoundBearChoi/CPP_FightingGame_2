28119138c04dd752b063f294934da49e85907e85
RoundBearChoi <roundbeargames@gmail.com>
2023-05-13 18:52:35 +0900

    save

--- FightingGameProject/JGetter.h
+++ FightingGameProject/JGetter.h
@@ -31,21 +31,6 @@ namespace RB::JSON
 			return loadedStr;
 		}
 
-		static json_object_element_s* GetFirstElement(const json_object_s& obj, std::string name)
-		{
-			json_object_element_s* firstElement = obj.start;
-			struct json_string_s* elementName = firstElement->name;
-
-			const char* nameChar = name.c_str();
-
-			if (strcmp(elementName->string, nameChar) == 0)
-			{
-				return firstElement;
-			}
-
-			return nullptr;
-		}
-
 		static json_object_element_s* GetElementN(const json_object_s& obj, int index)
 		{
 			json_object_element_s* element = obj.start;
@@ -62,21 +47,6 @@ namespace RB::JSON
 				element = element->next;
 				
 				count++;
-			}
-
-			return nullptr;
-		}
-
-		static json_object_element_s* GetNextElement(const json_object_element_s& element, std::string name)
-		{
-			json_object_element_s* nextElement = element.next;
-			struct json_string_s* elementName = nextElement->name;
-
-			const char* nameChar = name.c_str();
-
-			if (strcmp(elementName->string, nameChar) == 0)
-			{
-				return nextElement;
 			}
 
 			return nullptr;
@@ -106,13 +76,6 @@ namespace RB::JSON
 			return result;
 		}
 
-		static json_array_element_s* GetFirstArrayElement(const json_array_s& arr)
-		{
-			json_array_element_s* firstElement = arr.start;
-
-			return firstElement;
-		}
-
 		static int GetInt_FromArrayElement(const json_array_element_s& element)
 		{
 			struct json_number_s* number = json_value_as_number(element.value);
@@ -125,7 +88,7 @@ namespace RB::JSON
 			return result;
 		}
 
-		static float GetFloat_FromArrayAelement(const json_array_element_s& element)
+		static float GetFloat_FromArrayElement(const json_array_element_s& element)
 		{
 			struct json_number_s* number = json_value_as_number(element.value);
 
@@ -135,13 +98,6 @@ namespace RB::JSON
 			stream >> result;
 
 			return result;
-		}
-
-		static json_array_element_s* GetNextArrayElement(const json_array_element_s& element)
-		{
-			json_array_element_s* nextElement = element.next;
-
-			return nextElement;
 		}
 	};
 }

da203fca83d82602c52d3c7097eb071b13e7b782
RoundBearChoi <roundbeargames@gmail.com>
2023-05-13 11:00:42 +0900

    save

--- FightingGameProject/HurtBoxDataLoader.cpp
+++ FightingGameProject/HurtBoxDataLoader.cpp
@@ -9,6 +9,21 @@ namespace RB::HurtBox
 
 	void HurtBoxDataLoader::SaveSample()
 	{
+		std::string path = "HurtBoxData/";
 
+		path += "Sample.HurtBoxData";
+
+		std::ofstream file(path);
+
+		if (file.is_open())
+		{
+			file << "{" << std::endl;
+			file << "\"a\" : true," << std::endl;
+			file << "\"b\" : [false, null, \"foo\", 123, 3.14]" << std::endl;
+			file << "}" << std::endl;
+
+			file.flush();
+			file.close();
+		}
 	}
 }
--- FightingGameProject/HurtBoxDataLoader.h
+++ FightingGameProject/HurtBoxDataLoader.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <fstream>
 
 namespace RB::HurtBox
 {

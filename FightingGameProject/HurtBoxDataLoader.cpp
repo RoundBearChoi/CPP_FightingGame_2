8e2fd6b3dc1b2ac6557a5fa922a8bebcd0301984
RoundBearChoi <roundbeargames@gmail.com>
2023-05-13 22:55:23 +0900

    re

--- FightingGameProject/HurtBoxDataLoader.cpp
+++ FightingGameProject/HurtBoxDataLoader.cpp
@@ -123,14 +123,14 @@ namespace RB::HurtBox
 			file << "\"posY\" : 2," << std::endl;
 			file << "\"width\" : 1.5," << std::endl;
 			file << "\"height\" : 2.5" << std::endl;
-			file << "}," << std::endl;
-			
-			file << "{" << std::endl;
-			file << "\"posX\" : 3," << std::endl;
-			file << "\"posY\" : 4," << std::endl;
-			file << "\"width\" : 3.5," << std::endl;
-			file << "\"height\" : 4.5" << std::endl;
 			file << "}" << std::endl;
+			
+			//file << "{" << std::endl;
+			//file << "\"posX\" : 3," << std::endl;
+			//file << "\"posY\" : 4," << std::endl;
+			//file << "\"width\" : 3.5," << std::endl;
+			//file << "\"height\" : 4.5" << std::endl;
+			//file << "}" << std::endl;
 			
 			file << "]," << std::endl;
 

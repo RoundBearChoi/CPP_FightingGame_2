f3e2f88da6fe8b3d31d2fac5524b4c9253db3a7a
RoundBearChoi <roundbeargames@gmail.com>
2023-05-16 17:14:03 +0900

    save

--- FightingGameProject/MenuController.cpp
+++ FightingGameProject/MenuController.cpp
@@ -11,18 +11,15 @@ namespace RB::HBE::Menu
 	{
 		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 320, 20 }, "HurtBoxEditor", olc::WHITE, 2);
 
-		//olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 60 }, "Home , End : prev/next sprite", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 80 }, "PgUp , PgDown : prev/next frame", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 100 }, "Ins : create box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 120 }, "Del : delete box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 140 }, "O , P : prev/next box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 160 }, "WASD : move box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 180 }, "Q : enlarge box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 200 }, "E : shrink box", olc::WHITE);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 220 }, "ENTER : save data", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 80 }, "PgUp, PgDown : prev/next frame", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 100 }, "Ins, Del : create/delete box", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 120 }, "O, P : prev/next box", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 140 }, "WASD : move box", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 160 }, "Q, E : enlarge/shrink box", olc::WHITE);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 180 }, "ENTER : save data", olc::WHITE);
 
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 260 }, "current animation: ", olc::YELLOW);
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 280 }, "current animation frame: ", olc::YELLOW);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 220 }, "current animation: ", olc::YELLOW);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 240 }, "current animation frame: ", olc::YELLOW);
 	}
 
 	void MenuController::OnFixedUpdate()

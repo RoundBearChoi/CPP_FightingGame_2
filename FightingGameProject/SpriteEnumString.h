22260d14742a1d7c7b8017c6d13d61fd894e1ae8
RoundBearChoi <roundbeargames@gmail.com>
2023-05-16 18:54:24 +0900

    SpriteEnumString

---
+++ FightingGameProject/SpriteEnumString.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include "SpriteEnum.h"
+
+namespace RB::Sprites
+{
+	class SpriteEnumString
+	{
+	public:
+		SpriteEnumString(SpriteEnum spriteEnum, std::string string)
+		{
+			_spriteEnum = spriteEnum;
+			_string = string;
+		}
+
+	public:
+		SpriteEnum GetEnum() { return _spriteEnum; }
+		std::string GetString() { return _string; }
+
+	private:
+		SpriteEnum _spriteEnum = SpriteEnum::NONE;
+		std::string _string = "";
+	};
+}

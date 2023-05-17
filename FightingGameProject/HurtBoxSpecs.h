c58743f8c523ec2839801e577d795fd26b730eb3
RoundBearChoi <roundbeargames@gmail.com>
2023-05-17 11:46:19 +0900

    private specs variables

--- FightingGameProject/HurtBoxSpecs.h
+++ FightingGameProject/HurtBoxSpecs.h
@@ -6,11 +6,42 @@ namespace RB::HurtBox
 	{
 	public:
 		HurtBoxSpecs() = default;
+
+		HurtBoxSpecs(int32_t x, int32_t y, float width, float weight)
+		{
+			_x = x;
+			_y = y;
+			_width = width;
+			_height = weight;
+		}
+
 		~HurtBoxSpecs() = default;
 
-		int32_t mX = 0;
-		int32_t mY = 0;
-		float mWidth = 0; //even numbers?
-		float mWeight = 0; //even numbers?
+	public:
+		const int32_t& GetX()
+		{
+			return _x;
+		}
+
+		const int32_t& GetY()
+		{
+			return _y;
+		}
+
+		const float& GetWidth()
+		{
+			return _width;
+		}
+
+		const float& GetHeight()
+		{
+			return _height;
+		}
+
+	private:
+		int32_t _x = 0;
+		int32_t _y = 0;
+		float _width = 0; //even numbers using int?
+		float _height = 0; //even numbers using int?
 	};
 }

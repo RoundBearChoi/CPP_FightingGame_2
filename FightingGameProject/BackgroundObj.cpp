63be0de72b848791fd6eb7d3552227d823f8978e
RoundBearChoi <roundbeargames@gmail.com>
2023-06-04 18:07:05 +0900

    background tiling

--- FightingGameProject/BackgroundObj.cpp
+++ FightingGameProject/BackgroundObj.cpp
@@ -5,7 +5,7 @@ namespace RB::Background
 	BackgroundObj::BackgroundObj(std::string path, RB::Sprites::SpriteEnum spriteEnum, float_t percentage)
 	{
 		_renderSize = olc::vf2d{ 1152.0f, 720.0f };
-		_position = olc::vf2d{ _renderSize.x * 0.5f * -1.0f, 0.0f };
+		_position = olc::vf2d{ _renderSize.x * 2.0f * -1.0f, 0.0f };
 
 		_spriteEnum = spriteEnum;
 		_percentage = percentage;
@@ -29,7 +29,11 @@ namespace RB::Background
 
 		float_t offsetX = _position.x + (_camControllerGetter.GetController()->GetCamObj()->GetPosition().x * _percentage);
 
-		_spriteRenderer.RenderSprite(_spriteEnum, _renderSize, olc::vf2d{ offsetX, _position.y }, olc::WHITE, RB::Sprites::PivotType::BOTTOM_LEFT);
+		//background horizontal tile
+		for (int32_t i = 0; i < 4; i++)
+		{
+			_spriteRenderer.RenderSprite(_spriteEnum, _renderSize, olc::vf2d{ offsetX + (_renderSize.x * i), _position.y }, olc::WHITE, RB::Sprites::PivotType::BOTTOM_LEFT);
+		}
 	}
 
 	void BackgroundObj::OnFixedUpdate()

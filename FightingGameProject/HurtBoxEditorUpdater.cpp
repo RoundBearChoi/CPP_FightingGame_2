a71888db41324ed85570323fc4a8b0d2de259aeb
RoundBearChoi <roundbeargames@gmail.com>
2023-09-08 15:04:36 +0900

    save

--- FightingGameProject/HurtBoxEditorUpdater.cpp
+++ FightingGameProject/HurtBoxEditorUpdater.cpp
@@ -1,5 +1,19 @@
 #include "HurtBoxEditorUpdater.h"
 
+#include "Player.h"
+#include "P0_Dummy.h"
+
+#include "ActiveControllers.h"
+#include "InputController.h"
+#include "SpriteDataController.h"
+#include "CamController.h"
+#include "PlayerController.h"
+#include "PlayerDebugController.h"
+#include "PlayerAnimationController.h"
+#include "MenuController.h"
+#include "HurtBoxDataController.h"
+#include "HurtBoxEditController.h"
+
 namespace RB::Updaters
 {
 	HurtBoxEditorUpdater::HurtBoxEditorUpdater()
--- FightingGameProject/HurtBoxEditorUpdater.h
+++ FightingGameProject/HurtBoxEditorUpdater.h
@@ -1,19 +1,6 @@
 #pragma once
 #include <iostream>
 #include "iUpdaterObj.h"
-#include "Player.h"
-#include "P0_Dummy.h"
-
-#include "ActiveControllers.h"
-#include "InputController.h"
-#include "SpriteDataController.h"
-#include "CamController.h"
-#include "PlayerController.h"
-#include "PlayerDebugController.h"
-#include "PlayerAnimationController.h"
-#include "MenuController.h"
-#include "HurtBoxDataController.h"
-#include "HurtBoxEditController.h"
 
 namespace RB::Updaters
 {

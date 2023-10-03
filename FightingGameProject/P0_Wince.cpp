be91936cad6606d0f238af1e11ae6f1b255cd880
RoundBearChoi <roundbeargames@gmail.com>
2023-10-03 14:44:04 +0900

    save

--- FightingGameProject/P0_Wince.cpp
+++ FightingGameProject/P0_Wince.cpp
@@ -8,9 +8,12 @@ namespace RB::PlayerStates
 
 		StandardInit(RB::Sprites::SpriteEnum::fighter_0_wince);
 
-		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(0, false, 4));
-		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(2, true, 4));
-		AddStateComponent(new RB::PlayerStateComponents::TriggerIdleOnFixedUpdateCount(30));
+		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(0, false, 3)); //back
+		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(1, true, 2)); //forward
+		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(4, true, 3)); //forward
+		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(5, false, 4)); //way back
+		AddStateComponent(new RB::PlayerStateComponents::MoveHorizontalOnFixedUpdateCount(8, false, 2)); // slightly back
+		AddStateComponent(new RB::PlayerStateComponents::TriggerIdleOnFixedUpdateCount(10));
 
 		EnterStateComponents();
 	}

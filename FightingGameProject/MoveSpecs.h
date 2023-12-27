1cf248606f5726848f6f4405a817649a8ba2ffa1
RoundBearChoi <roundbeargames@gmail.com>
2023-12-27 18:21:59 +0900

    save

--- FightingGameProject/MoveSpecs.h
+++ FightingGameProject/MoveSpecs.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <cstdint> // for int32_t
-#include <cmath>   // for float_t
-#include <cstddef> // for size_t
+//#include <cstdint> // for int32_t
+//#include <cmath>   // for float_t
+//#include <cstddef> // for size_t
 
 #include "CharacterType.h"
 
@@ -13,11 +13,11 @@ namespace RB::Players::Specs
 	public:
 		RB::Players::CharacterType mCharacterType = RB::Players::CharacterType::NONE;
 
-		int32_t		mWalk_Forward_Speed = 0;
-		int32_t		mWalk_Back_Speed = 0;
-		size_t		mJumpUp_totalFrames = 0;
-		float_t		mJumpUp_speedMultiplier = 0.0f;
-		size_t		mJumpUp_Forward_totalFrames = 0;
-		float_t		mJumpUp_Forward_speedMultiplier = 0.0f;
+		int					mWalk_Forward_Speed = 0;
+		int					mWalk_Back_Speed = 0;
+		unsigned int		mJumpUp_totalFrames = 0;
+		float				mJumpUp_speedMultiplier = 0.0f;
+		unsigned int		mJumpUp_Forward_totalFrames = 0;
+		float				mJumpUp_Forward_speedMultiplier = 0.0f;
 	};
 }

08a0ae06a55dc011876f5b6ce80ef12e2760c416
RoundBearChoi <roundbeargames@gmail.com>
2023-12-28 13:31:10 +0900

    save

--- FightingGameProject/Selector.h
+++ FightingGameProject/Selector.h
@@ -25,12 +25,18 @@ namespace RB
 
 		void OnFixedUpdate()
 		{
-			//_fixedUpdated = true;
+
+		}
+
+		void PushBack(T t)
+		{
+			_vec.push_back(t);
 		}
 
 	private:
 		bool _fixedUpdated = false;
 		bool _initialized = false;
 		std::vector<T>::const_iterator _iterator;
+		std::vector<T> _vec;
 	};
 }

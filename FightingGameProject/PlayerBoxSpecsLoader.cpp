cfeb0f1ac66ac82c5ac0e6f361d3d627faef99b4
RoundBearChoi <roundbeargames@gmail.com>
2023-12-30 17:52:44 +0900

    save

--- FightingGameProject/PlayerBoxSpecsLoader.cpp
+++ FightingGameProject/PlayerBoxSpecsLoader.cpp
@@ -18,9 +18,32 @@ namespace RB::Collisions
 		{
 			unsigned int frame = RB::JSON::ParseFrame(element->name->string);
 			
-			//json_array_s* arr = json_value_as_array(element->value);
-			//json_array_element_s* e = arr->start;
-			//json_object_s* obj0 = json_value_as_object(e->value);
+			json_array_s* arr = json_value_as_array(element->value);
+
+			if (arr == nullptr)
+			{
+				element = element->next;
+				continue;
+			}
+
+			json_array_element_s* arrayElement = arr->start;
+
+			if (arrayElement == nullptr)
+			{
+				element = element->next;
+				continue;
+			}
+
+			json_object_s* obj0 = json_value_as_object(arrayElement->value);
+			json_object_element_s* e0 = obj0->start;
+			json_object_element_s* e1 = e0->next;
+			json_object_element_s* e2 = e1->next;
+			json_object_element_s* e3 = e2->next;
+
+			float offsetX = RB::JSON::GetFloat_FromElement(*e0);
+			float offsetY = RB::JSON::GetFloat_FromElement(*e1);
+			float width = RB::JSON::GetFloat_FromElement(*e2);
+			float height = RB::JSON::GetFloat_FromElement(*e3);
 		
 			element = element->next;
 		}

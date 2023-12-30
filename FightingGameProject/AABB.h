a617953e896f6ccafeb40e94a8127f3a4b1b50e9
RoundBearChoi <roundbeargames@gmail.com>
2023-12-30 22:01:41 +0900

    save

--- FightingGameProject/AABB.h
+++ FightingGameProject/AABB.h
@@ -7,36 +7,22 @@ namespace RB::Collisions
 	{
 	public:
 		AABB() = default;
-
 		AABB(float bottomLeftX, float bottomLeftY, float width, float height);
-
 		~AABB() = default;
 
 	public:
 		void SetBottomLeft(float bottomLeftX, float bottomLeftY);
-
 		void SetWidthHeight(float width, float height);
-
 		void MoveX(float x);
-
 		void MoveY(float y);
-
 		void IncreaseWidth(float amount);
-
 		void IncreaseHeight(float amount);
-
 		olc::vf2d GetCenter();
-
 		olc::vf2d GetBottomLeft();
-
 		olc::vf2d GetWidthHeight();
-
 		bool IsCollidingAgainst(const AABB& other, olc::vf2d& collisionCenter);
-
 		olc::vf2d GetCollisionCenter(const AABB& other);
-
 		AABB GetWorldPos(const olc::vf2d& playerPos, const bool& otherPlayerIsOnRightSide);
-
 		void ForceAABB(AABB& aabb);
 
 	private:

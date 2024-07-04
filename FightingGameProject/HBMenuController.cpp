c5787e5243779bb2b6a4f7fb8e7a77e0d33bbcbe
RoundBearChoi <roundbeargames@gmail.com>
2024-07-04 20:58:18 +0900

    save

--- FightingGameProject/HBMenuController.cpp
+++ FightingGameProject/HBMenuController.cpp
@@ -34,7 +34,7 @@ namespace RB::HBox
 		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 160 }, "UHJK : enlarge/shrink box", olc::WHITE);
 		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 180 }, "ENTER : save data (saves the entire set)", olc::WHITE);
 
-		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 280 }, "current animation frame: " + std::to_string(_GetCurrentAnimationFrame()), olc::YELLOW);
+		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 280 }, "animation frame: " + std::to_string(_GetCurrentAnimationFrame()), olc::YELLOW);
 		olc::Renderer::ptrPGE->DrawString(olc::vi2d{ 10, 300 }, "FrameName: " + _GetFrameName(), olc::YELLOW);
 
 		RB::HBox::Loaded_HB_Data* data = _GetHBData();

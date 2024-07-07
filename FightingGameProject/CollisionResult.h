4d40809dc012c2ebe9413623081e5153d640abc7
Roundbeargames Choi <roundbeargames@gmail.com>
2024-07-07 16:35:42 +0900

    save

--- FightingGameProject/CollisionResult.h
+++ FightingGameProject/CollisionResult.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "olcPixelGameEngine.h"
 #include "SpriteType.h"
+#include "CollisionType.h"
 
 #include "iPlayer.h"
 
@@ -12,6 +13,7 @@ namespace RB::Collisions
 		RB::Players::iPlayer* mAttacker = nullptr;
 		RB::Players::iPlayer* mTarget = nullptr;
 		olc::vf2d mCollisionPoint = { 0, 0 };
+		CollisionType mCollisionType = CollisionType::NONE;
 		RB::Sprites::SpriteType mAttackerSpriteType = RB::Sprites::SpriteType::NONE;
 	};
 }
---
+++ FightingGameProject/CollisionType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace RB::Collisions
+{
+	enum class CollisionType
+	{
+		NONE,
+
+		LEGS,
+		BODY,
+		HEAD,
+
+		COUNT
+	};
+}

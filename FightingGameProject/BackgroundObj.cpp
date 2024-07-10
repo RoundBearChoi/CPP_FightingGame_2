295c652a18739c80e6ee4f71867f88497f3b07b7
RoundBearChoi <roundbeargames@gmail.com>
2024-07-10 22:16:48 +0900

    save

--- FightingGameProject/BackgroundObj.cpp
+++ FightingGameProject/BackgroundObj.cpp
@@ -6,8 +6,11 @@ namespace RB::Background
 {
 	BackgroundObj::BackgroundObj(std::string path, RB::Sprites::SpriteType spriteType, float percentage)
 	{
-		_renderSize = olc::vf2d{ 1152.0f, 736.0f };
-		_position = olc::vf2d{ _renderSize.x * 2.0f * -1.0f, 16.0f };
+		//_renderSize = olc::vf2d{ 1152.0f, 736.0f };
+		_imageWidth = 288;
+		_imageHeight = 184;
+		_renderScale = 4.0f;
+		_position = olc::vf2d{ _imageWidth * _renderScale * 2.0f * -1.0f, 16.0f };
 
 		_spriteType = spriteType;
 		_percentage = percentage;
@@ -32,7 +35,7 @@ namespace RB::Background
 		//background horizontal tile
 		for (int i = 0; i < 4; i++)
 		{
-			_spriteContainer.RenderSprite(_spriteType, _renderSize.x, _renderSize.y, olc::vf2d{ offsetX + (_renderSize.x * i), _position.y }, olc::WHITE, RB::Sprites::PivotType::BOTTOM_LEFT, true);
+			_spriteContainer.RenderSprite(_spriteType, _imageWidth * _renderScale, _imageHeight * _renderScale, olc::vf2d{ offsetX + (_imageWidth * _renderScale * i), _position.y }, olc::WHITE, RB::Sprites::PivotType::BOTTOM_LEFT, true);
 		}
 	}
 
--- FightingGameProject/BackgroundObj.h
+++ FightingGameProject/BackgroundObj.h
@@ -18,7 +18,10 @@ namespace RB::Background
 	private:
 		float _percentage = 0.0f;
 		RB::Sprites::SpriteType _spriteType = RB::Sprites::SpriteType::NONE;
-		olc::vf2d _renderSize = { 0.0f ,0.0f };
+		//olc::vf2d _renderSize = { 0.0f ,0.0f };
+		float _imageWidth = 0.0f;
+		float _imageHeight = 0.0f;
+		float _renderScale = 1.0f;
 		olc::vf2d _position = { 0.0f, 0.0f };
 		RB::Sprites::SpriteContainer _spriteContainer;
 	};

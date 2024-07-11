167755edf09f07304cb99dc043456c3f3b018164
RoundBearChoi <roundbeargames@gmail.com>
2024-07-11 16:52:50 +0900

    save

---
+++ FightingGameProject/Parser.cpp
@@ -0,0 +1,55 @@
+#include "Parser.h"
+
+#include <iostream>
+#include <fstream>
+
+namespace RB::JSON
+{
+	Parser::~Parser()
+	{
+		for (int i = 0; i < _vecLoadedRoots.size(); i++)
+		{
+			free(_vecLoadedRoots[i]);
+		}
+
+		_vecLoadedRoots.clear();
+	}
+
+	json_value_s* Parser::LoadJSON(std::string path)
+	{
+		std::ifstream ifs(path);
+
+		std::string str((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+
+		std::cout << std::endl;
+		std::cout << "loading JSON at path: " << path << std::endl;
+
+		if (str.empty())
+		{
+			std::cout << "file doesn't exist: " << path << std::endl;
+		}
+		else
+		{
+			//std::cout << loadedStr << std::endl;
+		}
+
+		str.erase(std::remove(str.begin(), str.end(), '\n'), str.cend());
+
+		json_value_s* root = json_parse(str.c_str(), str.size());
+
+		_vecLoadedRoots.push_back(root);
+
+		return root;
+	}
+
+	json_object_s* Parser::GetObj(int index)
+	{
+		if (_vecLoadedRoots.size() > index)
+		{
+			json_object_s* jObj = json_value_as_object(_vecLoadedRoots[index]);
+			return jObj;
+		}
+
+		return nullptr;
+	}
+}
---
+++ FightingGameProject/Parser.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "json.h"
+
+namespace RB::JSON
+{
+	class Parser
+	{
+	public:
+		~Parser();
+
+	public:
+		json_value_s* LoadJSON(std::string path);
+		json_object_s* GetObj(int index);
+
+	private:
+		std::vector<json_value_s*> _vecLoadedRoots;
+	};
+}

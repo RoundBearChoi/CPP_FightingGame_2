9713f8fdcc20eff8fb6bf7f7e4e5141d774e5bc7
Roundbeargames Choi <roundbeargames@gmail.com>
2024-07-20 11:06:15 +0900

    save

---
+++ src/AllocationCount.cpp
@@ -0,0 +1,35 @@
+#include "AllocationCount.h"
+
+#if ALLOC_COUNT
+
+size_t numObjects = 0;
+bool showAllocCount = false;
+bool onlyShowZeroCount = true;
+
+void* operator new(std::size_t size)
+{
+    ++numObjects;
+
+    return std::malloc(size);
+}
+
+void operator delete(void* ptr) NOEXCEPT
+{
+    --numObjects;
+
+    std::free(ptr);
+
+    if (showAllocCount)
+    {
+        if (onlyShowZeroCount && numObjects != 0)
+        {
+            // do nothing
+        }
+        else
+        {
+            std::cout << "allocation count: " << numObjects << std::endl;;
+        }
+    }
+}
+
+#endif
--- src/AllocationCount.h
+++ src/AllocationCount.h
@@ -10,40 +10,14 @@
 #define NOEXCEPT _GLIBCXX_USE_NOEXCEPT
 #endif
 
-// Rest of your code...
-
-
 #define ALLOC_COUNT true
 #if ALLOC_COUNT
 
-size_t numObjects = 0;
-bool showAllocCount = false;
-bool onlyShowZeroCount = true;
+extern size_t numObjects;
+extern bool showAllocCount;
+extern bool onlyShowZeroCount;
 
-void* operator new(std::size_t size)
-{
-    ++numObjects;
-
-    return std::malloc(size);
-}
-
-void operator delete(void* ptr) NOEXCEPT
-{
-    --numObjects;
-
-    std::free(ptr);
-
-    if (showAllocCount)
-    {
-        if (onlyShowZeroCount && numObjects != 0)
-        {
-            // do nothing
-        }
-        else
-        {
-            std::cout << "allocation count: " << numObjects << std::endl;;
-        }
-    }
-}
+void* operator new(std::size_t size);
+void operator delete(void* ptr) NOEXCEPT;
 
 #endif

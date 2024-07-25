b76b6e122b1d0ec23454d533fa1a63ddcbde28d9
Roundbeargames Choi <roundbeargames@gmail.com>
2024-07-25 15:13:31 +0900

    save

--- src/Sprites/FlipQuad.cpp
+++ src/Sprites/FlipQuad.cpp
@@ -33,17 +33,18 @@ namespace RB::Sprites
 		float c = cosf(angle);
 
 		// translate point back to origin:
-		point.x -= center.x;
-		point.y -= center.y;
+		Vector2 vec = point - center;
 
 		// rotate point
-		float xnew = point.x * c - point.y * s;
-		float ynew = point.x * s + point.y * c;
+		float x = vec.x * c - vec.y * s;
+		float y = vec.x * s + vec.y * c;
 
 		// translate point back:
-		point.x = xnew + center.x;
-		point.y = ynew + center.y;
-		return point;
+		Vector2 result;
+		result.x = x + center.x;
+		result.y = y + center.y;
+
+		return result;
 	}
 
 	std::array<Vector2, 4> RotateQuad(std::array<Vector2, 4>arr, float angle)

bfcc17afc2fc1ca3c7a828f73f0e84333d4a4e22
Roundbeargames Choi <roundbeargames@gmail.com>
2024-09-02 14:36:24 +0900

    save

--- src/RandomGenerator.cpp
+++ src/RandomGenerator.cpp
@@ -9,6 +9,7 @@ namespace RB
 
     int RandomGenerator::GetRand(int min, int max)
     {
+        // both min and max are included in the range of possible values
         std::uniform_int_distribution<> dis(min, max);
         return dis(gen);
     }

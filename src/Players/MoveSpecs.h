b0cca68473cf2b08d4388c3d201eae3f1acc9bd0
rbg <roundbeargames@gmail.com>
2024-10-23 10:27:34 +0900

    save

--- src/Players/MoveSpecs.h
+++ src/Players/MoveSpecs.h
@@ -11,8 +11,10 @@ namespace RB::Players
 
 		float				mWalk_Forward_Speed = 0;
 		float				mWalk_Back_Speed = 0;
+
 		unsigned int		mJumpUp_totalFrames = 0;
 		float				mJumpUp_speedMultiplier = 0.0f;
+		
 		unsigned int		mJumpForward_totalFrames = 0;
 		float				mJumpForward_horSpeedMultiplier = 0.0f;
 		float				mJumpForward_verSpeedMultiplier = 0.0f;

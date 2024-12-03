f87d2331ff13f4ee8ffe06c32a21576b098907dd
rbg <roundbeargames@gmail.com>
2024-12-03 15:36:42 +0900

    save

--- src/PlayerStateComponents/MarkCrouchingOnExit.cpp
+++ src/PlayerStateComponents/MarkCrouchingOnExit.cpp
@@ -12,8 +12,8 @@ namespace RB::PlayerStateComponents
 		RB::Players::iPlayer* player = RB::Players::iPlayerController::Get()->GetPlayerOnStateMachineID(_state->GetStateMachineID());
 
 		std::cout << std::endl;
-		std::cout << "player " << player->GetPlayerID_int() << " crouching: " << _crouching << std::endl;
+		//std::cout << "player " << player->GetPlayerID_int() << " crouching: " << _crouching << std::endl;
 
-		//player->MarkCrouching(_crouching);
+		player->MarkCrouching(_crouching);
 	}
 }

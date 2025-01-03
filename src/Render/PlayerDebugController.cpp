eb2f8da24b1db349a1657956ac93aba723b3d661
rbg <roundbeargames@gmail.com>
2025-01-03 14:54:14 +0900

    save

--- src/Render/PlayerDebugController.cpp
+++ src/Render/PlayerDebugController.cpp
@@ -1,4 +1,5 @@
 #include "PlayerDebugController.h"
+#include "PlayerInputRenderer.h"
 
 namespace RB::Render
 {
@@ -106,5 +107,32 @@ namespace RB::Render
 	void PlayerDebugController::_RenderHPBars()
 	{
 
+
+		float bar_x = 215.0f;
+		float bar_y = 5.0f;
+
+		float top_y_margin = 28.0f;
+		float center_x_margin = 20.0f;
+
+		float p0_center_x = (512.0f * 0.5f) - center_x_margin;
+		float p1_center_x = (512.0f * 0.5f) + center_x_margin;
+
+		_spriteContainer.RenderSprite(
+			RB::Sprites::SpriteType::player_hp_bar_white, 
+			bar_x,
+			bar_y,
+			RB::Vector2{ p0_center_x, top_y_margin },
+			olc::WHITE,
+			RB::Sprites::PivotType::BOTTOM_RIGHT, 
+			false);
+
+		_spriteContainer.RenderSprite(
+			RB::Sprites::SpriteType::player_hp_bar_white, 
+			bar_x,
+			bar_y,
+			RB::Vector2{ p1_center_x, top_y_margin },
+			olc::WHITE,
+			RB::Sprites::PivotType::BOTTOM_LEFT, 
+			false);
 	}
 }

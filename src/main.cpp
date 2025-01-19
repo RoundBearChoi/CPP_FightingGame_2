1b9363cca75f271231fe975221764205cd74ccd5
rbg <roundbeargames@gmail.com>
2025-01-19 17:53:16 +0900

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include "AllocationCount.h"
-//class LastCounter { public: LastCounter() { std::cout << "starting allocation count: " << numObjects << std::endl; } ~LastCounter() { std::cout << "final allocation count: " << numObjects << std::endl; } };
-//LastCounter last;
-
 #include "Game.h"
 
 int main()
 {
 	RB::Game game;
-
 	game.Run();
-	
+
 	std::cout << "ending program.. press enter to exit.." << std::endl;
 	std::cin.get();
 

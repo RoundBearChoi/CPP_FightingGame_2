2291f9f4eb314d2ebf5e281ad03f4d6999bac52e
rbg <roundbeargames@gmail.com>
2025-01-21 19:43:14 +0900

    save

--- src/Updaters/GameplayUpdaterSetup.cpp
+++ src/Updaters/GameplayUpdaterSetup.cpp
@@ -6,12 +6,12 @@ namespace RB::Updaters
 {
     GameplayUpdaterSetup::GameplayUpdaterSetup(UpdaterBase* updater)
     {
-        updater->AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER);
-
-        Controllers::iController* playerController = updater->GetController(Controllers::ControllerType::PLAYER_CONTROLLER);
-
-        Controllers::ControllerType tt = playerController->GetControllerType();
-
-        int n = 0;
+        //updater->AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER);
+        //
+        //Controllers::iController* playerController = updater->GetController(Controllers::ControllerType::PLAYER_CONTROLLER);
+        //
+        //Controllers::ControllerType tt = playerController->GetControllerType();
+        //
+        //int n = 0;
     }
 }

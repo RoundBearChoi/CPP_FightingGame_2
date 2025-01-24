f0001a3d2f0fda10ed9df34cb46ae5e1846138fe
rbg <roundbeargames@gmail.com>
2025-01-24 20:34:38 +0900

    save

--- src/Updaters/GameplayUpdaterSetup.cpp
+++ src/Updaters/GameplayUpdaterSetup.cpp
@@ -12,13 +12,13 @@ namespace RB::Updaters
 {
     GameplayUpdaterSetup::GameplayUpdaterSetup(UpdaterBase* updater)
     {
-        updater->AddController(new Background::BackgroundController(), Controllers::ControllerType::BACKGROUND_CONTROLLER);
+        auto backgroundController = updater->AddController(new Background::BackgroundController(), Controllers::ControllerType::BACKGROUND_CONTROLLER);
         auto playerController = static_cast<Players::iPlayerController*>(updater->AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER));
-        updater->AddController(new Render::PlayerDebugController(), Controllers::ControllerType::PLAYER_DEBUG_CONTROLLER);
-        updater->AddController(new Render::PlayerAnimationController(), Controllers::ControllerType::PLAYER_ANIMATION_CONTROLLER);
-        updater->AddController(new Collisions::PlayerBoxDataController, Controllers::ControllerType::PLAYER_BOX_DATA_CONTROLLER);
-        updater->AddController(new Players::SpecsController(), Controllers::ControllerType::SPECS_CONTROLLER);
-        updater->AddController(new Input::InputController(), Controllers::ControllerType::INPUT_CONTROLLER);
+        auto playerDebugController = updater->AddController(new Render::PlayerDebugController(), Controllers::ControllerType::PLAYER_DEBUG_CONTROLLER);
+        auto playerAnimationController = updater->AddController(new Render::PlayerAnimationController(), Controllers::ControllerType::PLAYER_ANIMATION_CONTROLLER);
+        auto playerBoxDataController = updater->AddController(new Collisions::PlayerBoxDataController, Controllers::ControllerType::PLAYER_BOX_DATA_CONTROLLER);
+        auto specsController = updater->AddController(new Players::SpecsController(), Controllers::ControllerType::SPECS_CONTROLLER);
+        auto inputController = updater->AddController(new Input::InputController(), Controllers::ControllerType::INPUT_CONTROLLER);
 
         int n = 0;
     }

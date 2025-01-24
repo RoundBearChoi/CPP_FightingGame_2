60ae92d6076daf2b4fa4407fe09adb3a7caff29d
rbg <roundbeargames@gmail.com>
2025-01-24 22:49:29 +0900

    save

--- src/Cam/iCamController.h
+++ src/Cam/iCamController.h
@@ -11,10 +11,6 @@ namespace RB::Cam
 	class iCamController : public Controllers::ControllerBase
 	{
 	public:
-		virtual void Init() = 0;
-		virtual void OnUpdate() = 0;
-		virtual void OnFixedUpdate() = 0;
-
 		virtual void SetZoom(float zoom) = 0;
 		virtual iCamObj* GetCamObj() = 0;
 		virtual void AllowManualControl(bool toggle) = 0;
--- src/Collisions/iGeneralHitStopController.h
+++ src/Collisions/iGeneralHitStopController.h
@@ -9,10 +9,6 @@ namespace RB::Collisions
 	class iGeneralHitStopController : public Controllers::ControllerBase
 	{
 	public:
-		virtual void Init() = 0;
-		virtual void OnUpdate() = 0;
-		virtual void OnFixedUpdate() = 0;
-
 		virtual bool SkipFrame() = 0;
 		virtual void AddSkipFrames(int frames) = 0;
 	};

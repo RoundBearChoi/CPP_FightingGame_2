bd77ff4d8a98790f872626b4946fd15645f57d10
rbg <roundbeargames@gmail.com>
2025-01-24 23:44:28 +0900

    save

--- src/Updaters/PlayerBoxEditorUpdater.cpp
+++ src/Updaters/PlayerBoxEditorUpdater.cpp
@@ -1,14 +1,12 @@
 #include "PlayerBoxEditorUpdater.h"
 
-#include "../Vector2.h"
-
 #include "../Players/PlayerController.h"
 #include "../Render/PlayerAnimationController.h"
-#include "../Collisions/PlayerBoxDataController.h"
-#include "../Cam/CamController.h"
 #include "../Render/PlayerDebugController.h"
-#include "../Input/InputController.h"
+#include "../Collisions/PlayerBoxDataController.h"
 #include "../Collisions/PlayerBoxEditController.h"
+#include "../Cam/CamController.h"
+#include "../Input/InputController.h"
 
 #include "../Fighter_0_States/F0_Dummy.h"
 

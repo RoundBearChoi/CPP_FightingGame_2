96b711016b9baaebe05412ddae4f38315c9b7bb3
rbg <roundbeargames@gmail.com>
2025-01-24 20:32:44 +0900

    save

--- src/Updaters/GameplayUpdaterSetup.cpp
+++ src/Updaters/GameplayUpdaterSetup.cpp
@@ -13,7 +13,7 @@ namespace RB::Updaters
     GameplayUpdaterSetup::GameplayUpdaterSetup(UpdaterBase* updater)
     {
         updater->AddController(new Background::BackgroundController(), Controllers::ControllerType::BACKGROUND_CONTROLLER);
-        updater->AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER);
+        auto playerController = static_cast<Players::iPlayerController*>(updater->AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER));
         updater->AddController(new Render::PlayerDebugController(), Controllers::ControllerType::PLAYER_DEBUG_CONTROLLER);
         updater->AddController(new Render::PlayerAnimationController(), Controllers::ControllerType::PLAYER_ANIMATION_CONTROLLER);
         updater->AddController(new Collisions::PlayerBoxDataController, Controllers::ControllerType::PLAYER_BOX_DATA_CONTROLLER);

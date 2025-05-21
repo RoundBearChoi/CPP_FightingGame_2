3397e6d23d951bbea1f32a704d93ef5a8e112819
rbg <roundbeargames@gmail.com>
2025-05-21 11:39:15 +0900

    save

---
+++ src/Cam/GetRelPos.cpp
@@ -0,0 +1,6 @@
+#include "GetRelPos.h"
+
+RB::Vector2 RB::Cam::GetRelPos(RB::Vector2 pos)
+{
+    return RB::Vector2();
+}
---
+++ src/Cam/GetRelPos.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../Vector2.h"
+
+namespace RB::Cam
+{
+    extern RB::Vector2 GetRelPos(RB::Vector2 pos);
+}

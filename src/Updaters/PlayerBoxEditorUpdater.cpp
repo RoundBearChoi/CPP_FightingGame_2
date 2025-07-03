d701272f73bb20e9d4d2c947348ac0df342f3c3f
RoundBearChoi <roundbeargames@gmail.com>
2025-07-04 00:24:18 +0900

    save

--- src/Updaters/PlayerBoxEditorUpdater.cpp
+++ src/Updaters/PlayerBoxEditorUpdater.cpp
@@ -29,17 +29,10 @@ namespace RB::Updaters
 		AddController(new Input::InputController(), Controllers::ControllerType::INPUT_CONTROLLER);
 		AddController(new Render::PlayerDebugController(), Controllers::ControllerType::PLAYER_DEBUG_CONTROLLER);
 		AddController(new Collisions::PlayerBoxEditController(), Controllers::ControllerType::PLAYER_BOX_EDIT_CONTROLLER);
+		AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER);
 
-		auto playerController = static_cast<Players::iPlayerController*>(AddController(new Players::PlayerController(), Controllers::ControllerType::PLAYER_CONTROLLER));
 		auto camController = static_cast<Cam::iCamController*>(AddController(new Cam::CamController(), Controllers::ControllerType::CAM_CONTROLLER));
 		
-		//Players::iPlayer* p0 = playerController->AddPlayer();
-
-		//p0->Init(Players::PlayerID::PLAYER_1, new Fighter_0_States::F0_Dummy(Sprites::SpriteType::fighter_0_idle));
-		//p0->SetPosition(Vector2{ 50.0f, 100.0f });
-		//p0->SetCharacterType(Players::CharacterType::AKU);
-		//p0->SetManualAnimationUpdate(true);
-
 		camController->SetZoom(1.0f);
 
 		_InitAllControllers();

dbd43a7532ebd03d0dea8ac7c24fbbe5a784d62f
RoundBearChoi <roundbeargames@gmail.com>
2025-08-30 17:17:28 +0900

    save

--- src/FixedTimer.h
+++ src/FixedTimer.h
@@ -54,7 +54,7 @@ namespace RB
 
 		void ShowFixedUpdateCount()
 		{
-			olc::Renderer::ptrPGE->DrawStringDecal({ 22, 3 }, "FixedUpdate Count: " + std::to_string(_fixedUpdateCountPerSec), olc::WHITE, { 0.65f, 0.65f });
+			olc::Renderer::ptrPGE->DrawStringDecal({ 22, 3 }, "Fixed Update Count: " + std::to_string(_fixedUpdateCountPerSec), olc::WHITE, { 0.65f, 0.65f });
 		}
 
 	private:
